1de7211de35c18fa82865522e8029cc5f522d0a3
Rony Braswell <ronybraswell@hotmail.com>
2021-06-23 23:12:58 -0400

    updated projects

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 export const projects = [
   {
-    title: "",
-    subtitle: "",
+    title: "Code Quiz",
+    subtitle: "HTML/CSS/JavaScript",
     description:
-      "",
+      "This is a timed coding quiz with multiple-choice questions. This app will run in the browser and features dynamically updated HTML and CSS powered by JavaScript code. It has a clean, polished, and responsive user interface. When the quiz begins, a timer starts. An incorrect answer will result in time subtracted from the clock. When all questions are answered or the timer reaches 0 then the game is over. Upon comp[letion you can save your initials and your score.",
     image: "./project-1.gif",
-    link: "",
+    link: "https://ronyelon.github.io/Code-Quiz/",
   },
   {
     title: "",
