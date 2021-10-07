9f4085faa81262bf5e263c3f2c276318d71cf273
Rony Braswell <ronybraswell@hotmail.com>
2021-10-07 13:56:50 -0400

    update portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import About from "./components/About";
-import Contact from "./components/Contact";
+//import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -14,7 +14,6 @@ export default function App() {
       <Projects />
       <Skills />
       <Testimonials />
-      <Contact />
     </main>
   );
 }
